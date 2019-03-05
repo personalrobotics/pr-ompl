1090b6922b1b46ed7bbc825abc96900a37ae7d2d
Sherdil Niyaz <sniyaz@berkeley.edu>
2019-03-04 19:41:28 -0500

    Draft boost_defs.

--- frechet/include/NNFrechet.hpp
+++ frechet/include/NNFrechet.hpp
@@ -27,7 +27,52 @@ namespace NNFrechet {
   /// The OMPL Planner class that implements the algorithm
   class NNFrechet: public ompl::base::Planner
   {
+    struct VProp
+    {
+      // To identify TPG nodes.
+      std::string name;
 
+      // Robot configuration.
+      ompl::base::State* state;
+
+      // End-effector pose.
+      Eigen::Isometry3d poseEE;
+
+      // For figuring weight of CPG nodes.
+      double frechetDistance;
+    }; // struct VProp
+
+    struct EProp
+    {
+      // TPG Edge Weight.
+      double length;
+
+      // Evaluation marker for LazySP.
+      bool evaluated;
+    }; // struct EProp
+
+    // Boost Graph definitions
+    typedef boost::adjacency_list<boost::hash_setS, boost::vecS, boost::bidirectionalS, VProp, EProp> Graph;
+    typedef boost::graph_traits<Graph>::vertex_descriptor Vertex;
+    typedef boost::graph_traits<Graph>::edge_descriptor Edge;
+    typedef boost::graph_traits<Graph>::vertex_iterator VertexIter;
+    typedef boost::graph_traits<Graph>::edge_iterator EdgeIter;
+    typedef boost::graph_traits<Graph>::adjacency_iterator NeighborIter;
+
+    // Boost Graph Property Map definitions
+    // Vertex Maps
+    typedef boost::property_map<Graph, boost::vertex_index_t VProp::*>::type VertexIndexMap;
+    typedef boost::property_map<Graph, std::string VProp::*>::type VPNameMap;
+    typedef boost::property_map<Graph, ompl::base::State* VProp::*>::type VPStateMap;
+    typedef boost::property_map<Graph, Eigen::Isometry3d VProp::*>::type VPPoseEEMap;
+    typedef boost::property_map<Graph, double VProp::*>::type VPFrechetDistanceMap;
+
+    // Edge Maps
+    typedef boost::property_map<Graph, boost::edge_index_t EProp::*>::type EdgeIndexMap;
+    typedef boost::property_map<Graph, double EProp::*>::type EPLengthMap;
+    typedef boost::property_map<Graph, double EProp::*>::type EPEvaluatedMap;
+
+    // TODO.
   };
 
 } // namespace LRAstar
